0e7957bee2293a8eb01fedd8b9e1ad45de90dad8
Ayo Osho <aosho44@gmail.com>
2022-08-18 17:31:08 +0100

    Update html, css and jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { ConvertCalculator } from './components/Input/Input';
+
+function App() {
+	return (
+		<>
+			<ConvertCalculator />
+		</>
+	);
+}
+
+export default App;
